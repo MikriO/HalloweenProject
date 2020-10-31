81d6ef09cdcbac6c9f7a433dd82c0c1d6e32b16c
MikriO <68971689+MikriO@users.noreply.github.com>
2020-10-31 14:34:38 -0400

    halloween commit

--- src/test/java/HalloweenTest.java
+++ src/test/java/HalloweenTest.java
@@ -1,5 +1,12 @@
 public class HalloweenTest {
     public static void main(String[] args) {
         System.out.println("I'm going to have a fun");
+        System.out.println("it's Halloween again, how time flies");
+
+        // there will be no treat and trick this year
+        //line 1
+        //line 2
+        //line 3
+
     }
 }
